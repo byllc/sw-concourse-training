efcf886a51b72794b86f541b75d630590628aaa8
Bill Chapman <billchapman@starkandwayne.com>
2018-10-04 00:03:40 -0400

    test

--- nginx_release/ci/tasks/upload-release.sh
+++ nginx_release/ci/tasks/upload-release.sh
@@ -10,12 +10,13 @@ export BOSH_DIRECTOR=https://10.200.192.0:25555/
 export BOSH_CLIENT=admin
 export BOSH_CLIENT_SECRET=agile-defense
 export BOSH_ENVIRONMENT=training-bosh
+export WORKSPACE=`pwd`
 
 cd source-code/nginx_release
 
 ls -lah
 
-bosh alias-env --ca-cert ~/cert-file/training-bosh.pem -e $BOSH_DIRECTOR training-bosh
+bosh alias-env --ca-cert ${WORKSPACE}/cert-file/training-bosh.pem -e ${BOSH_DIRECTOR} training-bosh
 
 bosh login
 
